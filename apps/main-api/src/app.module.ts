78ff9a537f09ab797f45c8e061ba9992c356cd6f
vodopad-ik <uladzislau.byshkala@innowise.com>
2026-09-13 15:15:40 +0200

    feat(main-api): enable multiSchema for auth/main/notification schemas

--- apps/main-api/src/app.module.ts
+++ apps/main-api/src/app.module.ts
@@ -4,7 +4,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true}), PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
   controllers: [AppController],
   providers: [],
 })
